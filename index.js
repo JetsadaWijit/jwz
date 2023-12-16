689ddcb6c0655cfbdf3c00337d3d7deedb8099fd
JetsadaWijit <56456776+JetsadaWijit@users.noreply.github.com>
2023-12-16 07:19:35 +0700

    change fs import to require

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
 const axios = require('axios');
+const fs = require('fs');
 
 ////////////////
 // Essential //
 //////////////
-import { readFileSync } from 'fs';
-
 function readPropertiesFile(filePath) {
-  const fileContent = readFileSync(filePath, 'utf-8');
-  const lines = fileContent.split('\n');
-  const config = {};
+    const fileContent = fs.readFileSync(filePath, 'utf-8');
+    const lines = fileContent.split('\n');
+    const config = {};
 
-  lines.forEach(line => {
-    const trimmedLine = line.trim();
-    if (trimmedLine && trimmedLine.includes('=')) {
-      const [key, ...valueParts] = trimmedLine.split('=');
-      const value = valueParts.join('=').trim();
-      config[key.trim()] = value;
-    }
-  });
+    lines.forEach(line => {
+        const trimmedLine = line.trim();
+        if (trimmedLine && trimmedLine.includes('=')) {
+        const [key, ...valueParts] = trimmedLine.split('=');
+        const value = valueParts.join('=').trim();
+        config[key.trim()] = value;
+        }
+    });
 
-  return config;
+    return config;
 }
 
 function replacePlaceholders(url, replacements) {
-  for (const placeholder in replacements) {
-    const placeholderValue = replacements[placeholder];
-    url = url.replace(new RegExp(`\\$\\{${placeholder}\\}`, 'g'), placeholderValue);
-  }
-  return url;
+    for (const placeholder in replacements) {
+        const placeholderValue = replacements[placeholder];
+        url = url.replace(new RegExp(`\\$\\{${placeholder}\\}`, 'g'), placeholderValue);
+    }
+    return url;
 }
 
 /////////////
