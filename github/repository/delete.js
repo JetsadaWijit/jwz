162ccb279780989754a76cc611b8ea6c39c7f2b8
JetsadaWijit <56456776+JetsadaWijit@users.noreply.github.com>
2023-12-18 10:25:02 +0700

    add comment

--- github/repository/delete.js
+++ github/repository/delete.js
@@ -6,6 +6,12 @@ const {
     replacePlaceholders
 } = require('../../essential');
 
+/*
+    @param org = String
+    @param repos = Array
+    @param token = String
+*/
+
 async function deleteGitHubRepos(org, repos, token) {
     try {
         // Get org repo URL
