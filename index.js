ad9514c978c434d34e0852059c09656fcb866e8b
JetsadaWijit <56456776+JetsadaWijit@users.noreply.github.com>
2023-12-21 10:40:21 +0700

    fix space

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const githubRpositoryPath = './github/repository';
 
 module.exports = {
-  /////////////
-  // GitHub //
-  ///////////
-  buildGitHubRepos: require(`${githubRpositoryPath}/build`),
-  inviteGitHubReposCollaborators: require(`${githubRpositoryPath}/invite`),
-  removeGitHubReposCollaborators: require(`${githubRpositoryPath}/remove`),
-  deleteGitHubRepos: require(`${githubRpositoryPath}/delete`),
-  getGitHubReleaseVersion: require(`${githubRpositoryPath}/release`)
+    /////////////
+    // GitHub //
+    ///////////
+    buildGitHubRepos: require(`${githubRpositoryPath}/build`),
+    inviteGitHubReposCollaborators: require(`${githubRpositoryPath}/invite`),
+    removeGitHubReposCollaborators: require(`${githubRpositoryPath}/remove`),
+    deleteGitHubRepos: require(`${githubRpositoryPath}/delete`),
+    getGitHubReleaseVersion: require(`${githubRpositoryPath}/release`)
 };
