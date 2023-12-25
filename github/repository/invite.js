1430401ab50bf0cb0114c4ca9a526327f768e858
JetsadaWijit <56456776+JetsadaWijit@users.noreply.github.com>
2023-12-25 09:23:36 +0700

    fix logic

--- github/repository/invite.js
+++ github/repository/invite.js
@@ -28,7 +28,7 @@ async function inviteCollaboratorsToRepos(org, repos, arrays, token) {
                 };
 
                 try {
-                    await axios.put(replacePlaceholders(config.repocollaboratorurl, replacements), {}, {
+                    await axios.put(replacePlaceholders(config.repocollaboratorurl, replacements), {
                         headers: {
                             Authorization: `Bearer ${token}`,
                             Accept: 'application/vnd.github.v3+json',
