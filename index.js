77988953182d7775e8f0890c2a04278116bbb49f
JetsadaWijit <56456776+JetsadaWijit@users.noreply.github.com>
2023-12-26 18:54:15 +0700

    del

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-const githubRpositoryPath = './github/repository';
-
-module.exports = {
-    /////////////
-    // GitHub //
-    ///////////
-    buildGitHubRepos: require(`${githubRpositoryPath}/build`),
-    inviteGitHubReposCollaborators: require(`${githubRpositoryPath}/invite`),
-    removeGitHubReposCollaborators: require(`${githubRpositoryPath}/remove`),
-    deleteGitHubRepos: require(`${githubRpositoryPath}/delete`),
-    getGitHubReleaseVersion: require(`${githubRpositoryPath}/release`)
-};
+// Blank
